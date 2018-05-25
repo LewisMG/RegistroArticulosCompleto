a85a04f694b262eb389889712982e238bbc2c78f
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2018-05-25 15:14:46 -0400

    Agrego el script, clase articulos perteneciente a entidades y instale entityframework y configure app.config

---
+++ RegistroCotizacionCompleto/Entidades/Articulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegistroCotizacionCompleto.Entidades
+{
+    public class Articulo
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public int Existencia { get; set; }
+        public int CantCotizada { get; set; }
+
+        public Articulo()
+        {
+            ArticuloId = 0;
+            FechaVencimiento = DateTime.Now;
+            Descripcion = String.Empty;
+            Precio = 0;
+            Existencia = 0;
+            CantCotizada = 0;
+        }
+    }
+}
